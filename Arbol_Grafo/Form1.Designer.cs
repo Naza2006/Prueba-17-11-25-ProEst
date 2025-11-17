17f533ec75e59c6912545197b543dd2358c8e67f
Naza2006 <nazario@uamv.edu.ni>
2025-11-17 04:59:35 -0600

    Codigo del arbol terminado

--- Arbol_Grafo/Form1.Designer.cs
+++ Arbol_Grafo/Form1.Designer.cs
@@ -30,20 +30,26 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tvArbolOrganizacional = new System.Windows.Forms.TreeView();
-            this.txtNombreNodo = new System.Windows.Forms.TextBox();
-            this.txtPadreNodo = new System.Windows.Forms.TextBox();
-            this.btnInsertarNodo = new System.Windows.Forms.Button();
-            this.btnEliminarNodo = new System.Windows.Forms.Button();
-            this.btnBuscarNodo = new System.Windows.Forms.Button();
-            this.txtBuscarNodo = new System.Windows.Forms.TextBox();
-            this.btnPreorden = new System.Windows.Forms.Button();
-            this.btnPostorden = new System.Windows.Forms.Button();
-            this.btnPorNiveles = new System.Windows.Forms.Button();
             this.txtResultadosArbol = new System.Windows.Forms.TextBox();
+            this.btnPorNiveles = new System.Windows.Forms.Button();
+            this.btnPostorden = new System.Windows.Forms.Button();
+            this.btnPreorden = new System.Windows.Forms.Button();
+            this.txtBuscarNodo = new System.Windows.Forms.TextBox();
+            this.btnBuscarNodo = new System.Windows.Forms.Button();
+            this.btnEliminarNodo = new System.Windows.Forms.Button();
+            this.btnInsertarNodo = new System.Windows.Forms.Button();
+            this.txtPadreNodo = new System.Windows.Forms.TextBox();
+            this.txtNombreNodo = new System.Windows.Forms.TextBox();
+            this.tvArbolOrganizacional = new System.Windows.Forms.TreeView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtNombreEdificio = new System.Windows.Forms.TextBox();
+            this.btnAgregarEdificio = new System.Windows.Forms.Button();
+            this.lstEdificios = new System.Windows.Forms.ListBox();
+            this.cmbOrigen = new System.Windows.Forms.ComboBox();
+            this.cmbDestino = new System.Windows.Forms.ComboBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -77,88 +83,12 @@
             this.tabPage1.Text = "Arbol";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // txtResultadosArbol
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(338, 363);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Grafo";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tvArbolOrganizacional
-            // 
-            this.tvArbolOrganizacional.Location = new System.Drawing.Point(16, 86);
-            this.tvArbolOrganizacional.Name = "tvArbolOrganizacional";
-            this.tvArbolOrganizacional.Size = new System.Drawing.Size(183, 261);
-            this.tvArbolOrganizacional.TabIndex = 0;
-            // 
-            // txtNombreNodo
-            // 
-            this.txtNombreNodo.Location = new System.Drawing.Point(16, 49);
-            this.txtNombreNodo.Name = "txtNombreNodo";
-            this.txtNombreNodo.Size = new System.Drawing.Size(100, 20);
-            this.txtNombreNodo.TabIndex = 1;
-            // 
-            // txtPadreNodo
-            // 
-            this.txtPadreNodo.Location = new System.Drawing.Point(16, 23);
-            this.txtPadreNodo.Name = "txtPadreNodo";
-            this.txtPadreNodo.Size = new System.Drawing.Size(100, 20);
-            this.txtPadreNodo.TabIndex = 2;
-            // 
-            // btnInsertarNodo
-            // 
-            this.btnInsertarNodo.Location = new System.Drawing.Point(124, 33);
-            this.btnInsertarNodo.Name = "btnInsertarNodo";
-            this.btnInsertarNodo.Size = new System.Drawing.Size(75, 23);
-            this.btnInsertarNodo.TabIndex = 3;
-            this.btnInsertarNodo.Text = "Insertar";
-            this.btnInsertarNodo.UseVisualStyleBackColor = true;
-            // 
-            // btnEliminarNodo
-            // 
-            this.btnEliminarNodo.Location = new System.Drawing.Point(244, 86);
-            this.btnEliminarNodo.Name = "btnEliminarNodo";
-            this.btnEliminarNodo.Size = new System.Drawing.Size(75, 23);
-            this.btnEliminarNodo.TabIndex = 4;
-            this.btnEliminarNodo.Text = "Eliminar";
-            this.btnEliminarNodo.UseVisualStyleBackColor = true;
-            // 
-            // btnBuscarNodo
-            // 
-            this.btnBuscarNodo.Location = new System.Drawing.Point(244, 49);
-            this.btnBuscarNodo.Name = "btnBuscarNodo";
-            this.btnBuscarNodo.Size = new System.Drawing.Size(75, 23);
-            this.btnBuscarNodo.TabIndex = 5;
-            this.btnBuscarNodo.Text = "Buscar";
-            this.btnBuscarNodo.UseVisualStyleBackColor = true;
-            // 
-            // txtBuscarNodo
-            // 
-            this.txtBuscarNodo.Location = new System.Drawing.Point(219, 23);
-            this.txtBuscarNodo.Name = "txtBuscarNodo";
-            this.txtBuscarNodo.Size = new System.Drawing.Size(100, 20);
-            this.txtBuscarNodo.TabIndex = 6;
-            // 
-            // btnPreorden
-            // 
-            this.btnPreorden.Location = new System.Drawing.Point(244, 146);
-            this.btnPreorden.Name = "btnPreorden";
-            this.btnPreorden.Size = new System.Drawing.Size(75, 23);
-            this.btnPreorden.TabIndex = 7;
-            this.btnPreorden.Text = "Preorden";
-            this.btnPreorden.UseVisualStyleBackColor = true;
-            // 
-            // btnPostorden
-            // 
-            this.btnPostorden.Location = new System.Drawing.Point(244, 184);
-            this.btnPostorden.Name = "btnPostorden";
-            this.btnPostorden.Size = new System.Drawing.Size(75, 23);
-            this.btnPostorden.TabIndex = 8;
-            this.btnPostorden.Text = "Postorden";
-            this.btnPostorden.UseVisualStyleBackColor = true;
+            this.txtResultadosArbol.Location = new System.Drawing.Point(18, 323);
+            this.txtResultadosArbol.Name = "txtResultadosArbol";
+            this.txtResultadosArbol.Size = new System.Drawing.Size(314, 20);
+            this.txtResultadosArbol.TabIndex = 10;
             // 
             // btnPorNiveles
             // 
@@ -169,12 +99,133 @@
             this.btnPorNiveles.Text = "Niveles";
             this.btnPorNiveles.UseVisualStyleBackColor = true;
             // 
-            // txtResultadosArbol
+            // btnPostorden
             // 
-            this.txtResultadosArbol.Location = new System.Drawing.Point(219, 264);
-            this.txtResultadosArbol.Name = "txtResultadosArbol";
-            this.txtResultadosArbol.Size = new System.Drawing.Size(100, 20);
-            this.txtResultadosArbol.TabIndex = 10;
+            this.btnPostorden.Location = new System.Drawing.Point(244, 184);
+            this.btnPostorden.Name = "btnPostorden";
+            this.btnPostorden.Size = new System.Drawing.Size(75, 23);
+            this.btnPostorden.TabIndex = 8;
+            this.btnPostorden.Text = "Postorden";
+            this.btnPostorden.UseVisualStyleBackColor = true;
+            // 
+            // btnPreorden
+            // 
+            this.btnPreorden.Location = new System.Drawing.Point(244, 146);
+            this.btnPreorden.Name = "btnPreorden";
+            this.btnPreorden.Size = new System.Drawing.Size(75, 23);
+            this.btnPreorden.TabIndex = 7;
+            this.btnPreorden.Text = "Preorden";
+            this.btnPreorden.UseVisualStyleBackColor = true;
+            // 
+            // txtBuscarNodo
+            // 
+            this.txtBuscarNodo.Location = new System.Drawing.Point(219, 23);
+            this.txtBuscarNodo.Name = "txtBuscarNodo";
+            this.txtBuscarNodo.Size = new System.Drawing.Size(100, 20);
+            this.txtBuscarNodo.TabIndex = 6;
+            // 
+            // btnBuscarNodo
+            // 
+            this.btnBuscarNodo.Location = new System.Drawing.Point(244, 49);
+            this.btnBuscarNodo.Name = "btnBuscarNodo";
+            this.btnBuscarNodo.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscarNodo.TabIndex = 5;
+            this.btnBuscarNodo.Text = "Buscar";
+            this.btnBuscarNodo.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarNodo
+            // 
+            this.btnEliminarNodo.Location = new System.Drawing.Point(244, 86);
+            this.btnEliminarNodo.Name = "btnEliminarNodo";
+            this.btnEliminarNodo.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarNodo.TabIndex = 4;
+            this.btnEliminarNodo.Text = "Eliminar";
+            this.btnEliminarNodo.UseVisualStyleBackColor = true;
+            // 
+            // btnInsertarNodo
+            // 
+            this.btnInsertarNodo.Location = new System.Drawing.Point(124, 23);
+            this.btnInsertarNodo.Name = "btnInsertarNodo";
+            this.btnInsertarNodo.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertarNodo.TabIndex = 3;
+            this.btnInsertarNodo.Text = "Insertar";
+            this.btnInsertarNodo.UseVisualStyleBackColor = true;
+            // 
+            // txtPadreNodo
+            // 
+            this.txtPadreNodo.Location = new System.Drawing.Point(18, 51);
+            this.txtPadreNodo.Name = "txtPadreNodo";
+            this.txtPadreNodo.Size = new System.Drawing.Size(100, 20);
+            this.txtPadreNodo.TabIndex = 2;
+            // 
+            // txtNombreNodo
+            // 
+            this.txtNombreNodo.Location = new System.Drawing.Point(18, 23);
+            this.txtNombreNodo.Name = "txtNombreNodo";
+            this.txtNombreNodo.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreNodo.TabIndex = 1;
+            // 
+            // tvArbolOrganizacional
+            // 
+            this.tvArbolOrganizacional.Location = new System.Drawing.Point(16, 86);
+            this.tvArbolOrganizacional.Name = "tvArbolOrganizacional";
+            this.tvArbolOrganizacional.Size = new System.Drawing.Size(183, 231);
+            this.tvArbolOrganizacional.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.cmbDestino);
+            this.tabPage2.Controls.Add(this.cmbOrigen);
+            this.tabPage2.Controls.Add(this.lstEdificios);
+            this.tabPage2.Controls.Add(this.btnAgregarEdificio);
+            this.tabPage2.Controls.Add(this.txtNombreEdificio);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(338, 363);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Grafo";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtNombreEdificio
+            // 
+            this.txtNombreEdificio.Location = new System.Drawing.Point(47, 22);
+            this.txtNombreEdificio.Name = "txtNombreEdificio";
+            this.txtNombreEdificio.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreEdificio.TabIndex = 0;
+            // 
+            // btnAgregarEdificio
+            // 
+            this.btnAgregarEdificio.Location = new System.Drawing.Point(47, 48);
+            this.btnAgregarEdificio.Name = "btnAgregarEdificio";
+            this.btnAgregarEdificio.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarEdificio.TabIndex = 1;
+            this.btnAgregarEdificio.Text = "Agregar";
+            this.btnAgregarEdificio.UseVisualStyleBackColor = true;
+            // 
+            // lstEdificios
+            // 
+            this.lstEdificios.FormattingEnabled = true;
+            this.lstEdificios.Location = new System.Drawing.Point(48, 110);
+            this.lstEdificios.Name = "lstEdificios";
+            this.lstEdificios.Size = new System.Drawing.Size(120, 95);
+            this.lstEdificios.TabIndex = 2;
+            // 
+            // cmbOrigen
+            // 
+            this.cmbOrigen.FormattingEnabled = true;
+            this.cmbOrigen.Location = new System.Drawing.Point(48, 227);
+            this.cmbOrigen.Name = "cmbOrigen";
+            this.cmbOrigen.Size = new System.Drawing.Size(121, 21);
+            this.cmbOrigen.TabIndex = 3;
+            // 
+            // cmbDestino
+            // 
+            this.cmbDestino.FormattingEnabled = true;
+            this.cmbDestino.Location = new System.Drawing.Point(48, 264);
+            this.cmbDestino.Name = "cmbDestino";
+            this.cmbDestino.Size = new System.Drawing.Size(121, 21);
+            this.cmbDestino.TabIndex = 4;
             // 
             // Form1
             // 
@@ -187,6 +238,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -207,6 +260,11 @@
         private System.Windows.Forms.Button btnPorNiveles;
         private System.Windows.Forms.Button btnPostorden;
         private System.Windows.Forms.Button btnPreorden;
+        private System.Windows.Forms.ComboBox cmbDestino;
+        private System.Windows.Forms.ComboBox cmbOrigen;
+        private System.Windows.Forms.ListBox lstEdificios;
+        private System.Windows.Forms.Button btnAgregarEdificio;
+        private System.Windows.Forms.TextBox txtNombreEdificio;
     }
 }
 
--- Arbol_Grafo/Form1.cs
+++ Arbol_Grafo/Form1.cs
@@ -10,11 +10,253 @@ using System.Windows.Forms;
 
 namespace Arbol_Grafo
 {
+    
     public partial class Form1 : Form
     {
+        NodoArbol raiz; // raíz del árbol
+
         public Form1()
         {
             InitializeComponent();
+
+            // Asegúrate de que estos eventos estén conectados
+            btnInsertarNodo.Click += btnInsertarNodo_Click;
+            btnEliminarNodo.Click += btnEliminarNodo_Click;
+            btnBuscarNodo.Click += btnBuscarNodo_Click;
+            btnPreorden.Click += btnPreorden_Click;
+            btnPostorden.Click += btnPostorden_Click;
+            btnPorNiveles.Click += btnPorNiveles_Click;
+        }
+
+        // ================== LÓGICA BÁSICA DEL ÁRBOL ==================
+
+        NodoArbol BuscarNodo(NodoArbol actual, string nombre)
+        {
+            if (actual == null) return null;
+            if (string.Equals(actual.Nombre, nombre, StringComparison.OrdinalIgnoreCase))
+                return actual;
+
+            foreach (var h in actual.Hijos)
+            {
+                var r = BuscarNodo(h, nombre);
+                if (r != null) return r;
+            }
+            return null;
+        }
+
+        bool EliminarNodo(NodoArbol actual, string nombre)
+        {
+            // intenta eliminar en la lista de hijos
+            for (int i = 0; i < actual.Hijos.Count; i++)
+            {
+                if (string.Equals(actual.Hijos[i].Nombre, nombre, StringComparison.OrdinalIgnoreCase))
+                {
+                    actual.Hijos.RemoveAt(i);
+                    return true;
+                }
+                if (EliminarNodo(actual.Hijos[i], nombre))
+                    return true;
+            }
+            return false;
+        }
+
+        void RefrescarTreeView()
+        {
+            tvArbolOrganizacional.Nodes.Clear();
+            if (raiz != null)
+                CrearNodoTree(raiz, tvArbolOrganizacional.Nodes);
+            tvArbolOrganizacional.ExpandAll();
+        }
+
+        void CrearNodoTree(NodoArbol nodo, TreeNodeCollection col)
+        {
+            TreeNode tn = new TreeNode(nodo.Nombre);
+            col.Add(tn);
+            foreach (var h in nodo.Hijos)
+                CrearNodoTree(h, tn.Nodes);
+        }
+
+        // ================== BOTONES ==================
+
+        void btnInsertarNodo_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombreNodo.Text.Trim();
+            string padre = txtPadreNodo.Text.Trim();
+
+            if (string.IsNullOrEmpty(nombre))
+            {
+                txtResultadosArbol.Text = "Ingrese un nombre de nodo.";
+                return;
+            }
+
+            // Si no hay raíz, este será la raíz
+            if (raiz == null)
+            {
+                raiz = new NodoArbol(nombre);
+                RefrescarTreeView();
+                return;
+            }
+
+            // Insertar debajo de un padre
+            if (string.IsNullOrEmpty(padre))
+            {
+                txtResultadosArbol.Text = "Para insertar hijos, escriba el nombre del padre.";
+                return;
+            }
+
+            var nodoPadre = BuscarNodo(raiz, padre);
+            if (nodoPadre == null)
+            {
+                txtResultadosArbol.Text = $"No se encontró el padre \"{padre}\".";
+                return;
+            }
+
+            // Evitar duplicado simple
+            if (BuscarNodo(raiz, nombre) != null)
+            {
+                txtResultadosArbol.Text = $"Ya existe un nodo llamado \"{nombre}\".";
+                return;
+            }
+
+            nodoPadre.Hijos.Add(new NodoArbol(nombre));
+            RefrescarTreeView();
+            txtNombreNodo.Clear();
+            txtPadreNodo.Clear();
+        }
+
+        void btnEliminarNodo_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombreNodo.Text.Trim();
+            if (string.IsNullOrEmpty(nombre))
+            {
+                txtResultadosArbol.Text = "Escriba el nombre del nodo a eliminar en txtNombreNodo.";
+                return;
+            }
+
+            if (raiz == null)
+            {
+                txtResultadosArbol.Text = "El árbol está vacío.";
+                return;
+            }
+
+            // Si es la raíz
+            if (string.Equals(raiz.Nombre, nombre, StringComparison.OrdinalIgnoreCase))
+            {
+                raiz = null;
+                RefrescarTreeView();
+                txtResultadosArbol.Text = "Se eliminó la raíz.";
+                return;
+            }
+
+            bool eliminado = EliminarNodo(raiz, nombre);
+            RefrescarTreeView();
+            txtResultadosArbol.Text = eliminado ?
+                $"Nodo \"{nombre}\" eliminado." :
+                $"No se encontró el nodo \"{nombre}\".";
+        }
+
+        void btnBuscarNodo_Click(object sender, EventArgs e)
+        {
+            string nombre = txtBuscarNodo.Text.Trim();
+            if (string.IsNullOrEmpty(nombre))
+            {
+                txtResultadosArbol.Text = "Escriba el nombre a buscar.";
+                return;
+            }
+
+            if (raiz == null)
+            {
+                txtResultadosArbol.Text = "El árbol está vacío.";
+                return;
+            }
+
+            var encontrado = BuscarNodo(raiz, nombre);
+            txtResultadosArbol.Text = encontrado != null
+                ? $"Nodo \"{nombre}\" encontrado."
+                : $"Nodo \"{nombre}\" no encontrado.";
+        }
+
+        void btnPreorden_Click(object sender, EventArgs e)
+        {
+            if (raiz == null)
+            {
+                txtResultadosArbol.Text = "El árbol está vacío.";
+                return;
+            }
+
+            List<string> nodos = new List<string>();
+            Preorden(raiz, nodos);
+            txtResultadosArbol.Text = "Preorden:\r\n" + string.Join(" -> ", nodos);
+        }
+
+        void btnPostorden_Click(object sender, EventArgs e)
+        {
+            if (raiz == null)
+            {
+                txtResultadosArbol.Text = "El árbol está vacío.";
+                return;
+            }
+
+            List<string> nodos = new List<string>();
+            Postorden(raiz, nodos);
+            txtResultadosArbol.Text = "Postorden:\r\n" + string.Join(" -> ", nodos);
+        }
+
+        void btnPorNiveles_Click(object sender, EventArgs e)
+        {
+            if (raiz == null)
+            {
+                txtResultadosArbol.Text = "El árbol está vacío.";
+                return;
+            }
+
+            List<string> nodos = new List<string>();
+            PorNiveles(raiz, nodos);
+            txtResultadosArbol.Text = "Por niveles:\r\n" + string.Join(" -> ", nodos);
+        }
+
+        // ================== RECORRIDOS ==================
+
+        void Preorden(NodoArbol n, List<string> lista)
+        {
+            if (n == null) return;
+            lista.Add(n.Nombre);
+            foreach (var h in n.Hijos)
+                Preorden(h, lista);
+        }
+
+        void Postorden(NodoArbol n, List<string> lista)
+        {
+            if (n == null) return;
+            foreach (var h in n.Hijos)
+                Postorden(h, lista);
+            lista.Add(n.Nombre);
+        }
+
+        void PorNiveles(NodoArbol n, List<string> lista)
+        {
+            Queue<NodoArbol> q = new Queue<NodoArbol>();
+            q.Enqueue(n);
+            while (q.Count > 0)
+            {
+                var x = q.Dequeue();
+                lista.Add(x.Nombre);
+                foreach (var h in x.Hijos)
+                    q.Enqueue(h);
+            }
         }
     }
+
+    public class NodoArbol
+    {
+        public string Nombre;
+        public List<NodoArbol> Hijos = new List<NodoArbol>();
+
+        public NodoArbol(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+
+    
 }
